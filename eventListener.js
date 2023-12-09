a1d5083a909bb014205567b1733681de287155c1
Eric <70970042+devevh@users.noreply.github.com>
2023-12-09 09:55:27 +0100

    désactivation de la modale d'aide

--- eventListener.js
+++ eventListener.js
@@ -3,6 +3,6 @@ document.getElementById('accueil').addEventListener('click', function(){ window.
 //clic sur le bouton pour quitter l'application
 document.getElementById('quitter').addEventListener('click', function(){ window.close()});
 //clic sur aide pour afficher la modale d'aide
-document.getElementById('aide').addEventListener('click',function(){document.getElementById('modaleAide').style.display='block';});
+//document.getElementById('aide').addEventListener('click',function(){document.getElementById('modaleAide').style.display='block';});
 //clic sur le x pour fermer la modale de aide
-document.getElementById('fermerAide').addEventListener('click',function(){document.getElementById('modaleAide').style.display='none';});
+//document.getElementById('fermerAide').addEventListener('click',function(){document.getElementById('modaleAide').style.display='none';});
